63ba9745430813e17ab3567069012755fab5e541
Ural Erkut <ural.erkut1@gmail.com>
2022-08-18 22:10:50 +0200

    extend code

--- bettercalldimi.py
+++ bettercalldimi.py
@@ -4,8 +4,12 @@ import webbrowser
 
 bettercalldimi = 'https://imgur.com/a/3hKNLTl'
 
+
 print('If you want to call Dimi, press ALT+SHIFT+D')
 
-
-time.sleep(5)
-
+if keyboard.is_pressed('alt+shift+d'):
+    print('Ring Ring! Calling Dimi...')
+    time.sleep(5)
+    webbrowser.open(bettercalldimi)
+else:
+    print('Hmm looks like Dimi is feeding Stella again ¯\_(ツ)_/¯')

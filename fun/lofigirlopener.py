f8a3642104a5603021313392d1e1a23f1c2323b6
Ural Erkut <ural.erkut1@gmail.com>
2022-08-19 01:43:27 +0200

    update, add lofigirlopener

--- fun/lofigirlopener.py
+++ fun/lofigirlopener.py
@@ -2,17 +2,17 @@ import webbrowser
 import feedparser
 from random import choice
 
-def zupay():
+def lofi():
 
     # Get channel url
-    channel_url = feedparser.parse("https://www.youtube.com/feeds/videos.xml?channel_id=UCNkIDDoZBwbDG8yQ9IHUN9Q")
+    channel_url = feedparser.parse("https://www.youtube.com/feeds/videos.xml?channel_id=UCSJ4gkVC6NrvII8umztf0Ow")
 
     # Grab random video
     video = choice(channel_url.entries)
 
     # Open video
-    print('Playing random song from Zupay...')
+    print('Playing random song from LofiGirl...')
     webbrowser.open(video.link)
 
 if __name__ == '__main__':
-    zupay()
+    lofi()

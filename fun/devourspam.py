7bb60ab440b370cb744240b170cb3ba94b572769
Ural Erkut <ural.erkut1@gmail.com>
2022-08-19 00:31:49 +0200

    add more numbers to devourspam

--- fun/devourspam.py
+++ fun/devourspam.py
@@ -3,9 +3,17 @@ import time
 
 while True:
     try:
-        for i in range(3):
-            time.sleep(0.1)
+        for i in range(5):
+            time.sleep(1)
             pyautogui.press("1")
+            time.sleep(1)
+            pyautogui.press("2")
+            time.sleep(1)
+            pyautogui.press("3")
+            time.sleep(1)
+            pyautogui.press("4")
+            time.sleep(1)
+
 
     except KeyboardInterrupt:
         print('Exiting...')

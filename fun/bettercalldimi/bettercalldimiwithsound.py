ccb36baf5234d34f0371bb884585b0ccf49076d9
Ural Erkut <ural.erkut1@gmail.com>
2022-08-19 00:48:38 +0200

    add new bettercalldimi version with nokia sound

---
+++ fun/bettercalldimi/bettercalldimiwithsound.py
@@ -0,0 +1,19 @@
+import keyboard
+import time
+import webbrowser
+from playsound import playsound
+
+bettercalldimi = 'https://i.imgur.com/SacLumd.mp4'
+
+print('If you want to call Dimi, press d')
+while True:
+    try:
+        comb = keyboard.is_pressed('d')
+        if comb:
+            call = print('Ring Ring! Calling Dimi...')
+            playsound('bettercalldimi/assets/note.mp3')
+            webbrowser.open(bettercalldimi)
+            break
+    except KeyboardInterrupt: # ctrl+c to stop
+            print('Hmm looks like Dimi is feeding Stella again ¯\_(ツ)_/¯. Try again later.')
+            break

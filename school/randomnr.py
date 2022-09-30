239c599656dd1a9774364f53029b909c12375301
uralium <ural.erkut1@gmail.com>
2022-09-30 09:35:28 +0200

    update randomnr.py

--- school/randomnr.py
+++ school/randomnr.py
@@ -1,9 +1,26 @@
 # generate random floating point values
 from random import seed
 from random import random
+from random import randint
+
+def randomnumber():
 # seed random number generator
-seed(1)
+    seed(1)
 # generate random numbers between 0-1
-for _ in range(10):
-	x = 6*random()
-	print(x)
+    for _ in range(10):
+        x = 6*random()
+        print(x)
+
+# call function
+randomnumber()
+
+def randomint():
+# seed random number generator
+    seed(1)
+
+# generate some integers
+    for _ in range(10):
+        value = randint(0, 10)
+        print(value)
+# call function
+randomint()
